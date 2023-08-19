acc8d9836703f0340c80fd3416150114df0aad51
ahmeddysf100 <ahmedysf100@gmail.com>
2023-08-19 03:37:15 +0300

    www

--- src/ciphers/ahmed.mjs
+++ src/ciphers/ahmed.mjs
@@ -7,65 +7,86 @@ import { tap } from "./tap.mjs";
 
 
 function ahmed_enc(text, key) {
-    var matrix = [], func_loop = 0
+    var matriz = [], func_loop = 0
     text = text.split("")
     // console.log(text)
     for (var i = 0; i < text.length; i++) {
-        switch (func_loop) {
-            case 0:
-                var x = Cipher.encrypt(text[i], "5")
-                matrix.push(x.msg)
-                break
-            case 1:
-                var y = morse.encode(text[i])
-                matrix.push(y)
-                break
-            case 2:
-                var z = ahmed_otp.enc(text[i], key)
-                matrix.push(z)
-                break
-            case 3:
-                var w = tap.tap_enc_for_ahmedjs(text[i])
-                matrix.push(w)
-                break
+        if (text[i] === " ") {
+            matriz.push("a1a")
+            matriz.push("   ")
         }
-        matrix.push("   ")
-        func_loop = (func_loop + 1) % 4
+        else {
+            switch (func_loop) {
+                case 0:
+                    var x = Cipher.encrypt(text[i], "5")
+                    matriz.push(x.msg)
+                    break
+                case 1:
+                    var y = morse.encode(text[i])
+                    matriz.push(y)
+                    break
+                case 2:
+                    var z = ahmed_otp.enc(text[i], key)
+                    matriz.push(z)
+                    break
+                case 3:
+                    var w = tap.tap_enc_for_ahmedjs(text[i])
+                    matriz.push(w)
+                    break
+            }
+            matriz.push("   ")
+            func_loop = (func_loop + 1) % 4
+        }
     }
-    return matrix.join("")
+    return matriz.join("")
 }
 
+
 function ahmed_dec(cipher, key) {
-    var matrix = [], func_loop = 0
-    cipher = cipher.split("   ")
-    // console.log(cipher)
-    for (var i = 0; i < cipher.length; i++) {
-        switch (func_loop) {
-            case 0:
-                var x = Cipher.decrypt(cipher[i], "-5","")
-                matrix.push(x.msg)
-                break
-            case 1:
-                var y = morse.decode(cipher[i])
-                matrix.push(y)
-                break
-            case 2:
-                var z = ahmed_otp.dec(cipher[i], key)
-                matrix.push(z)
-                break
-            case 3:
-                var w = tap.tap_dec(cipher[i])
-                matrix.push(w)
-                break
+    var matrix = [], func_loop = 0, cipherR = cipher.split("   ")
+
+    // console.log(cipherR)
+
+    cipherR.splice(cipherR.indexOf(""), 1)
+
+    console.log(cipherR)
+    for (var i = 0; i < cipherR.length; i++) {
+
+        if (cipherR[i] === "a1a") {
+            matrix.push(" ")
         }
-        func_loop = (func_loop + 1) % 4
+        else {
+            switch (func_loop) {
+                case 0:
+                    var x = Cipher.decrypt(cipherR[i], "-5", "")
+                    matrix.push(x.msg)
+                    break
+                case 1:
+                    var y = morse.decode(cipherR[i])
+                    matrix.push(y)
+                    break
+                case 2:
+                    var z = ahmed_otp.dec(cipherR[i], key)
+                    matrix.push(z)
+                    break
+                case 3:
+                    var w = tap.tap_dec(cipherR[i])
+                    matrix.push(w)
+                    break
+            }
+
+            func_loop = (func_loop + 1) % 4
+            // console.log(matrix)
+        }
     }
     return matrix.join("")
 }
 // console.log(Cipher.encrypt("a",5).msg + "  " + Cipher.decrypt("F",-5,"").msg)
+// var zxz = ahmed_enc("HELLO I AM A LITTLE WORRIED THAT BESERK IS NOT ", "h")
 
-// console.log(ahmed_enc("ahmed gsdvsd sdvs sd", "h"))
-// console.log(ahmed_dec("F   ....   t   . .....   I   .......   n   .... ...   I   ...-   z   . ....       ...   k   ..... .   X   .......   z   . ....   ", "h"))
+
+
+
 
 
 export const ahmed = {
